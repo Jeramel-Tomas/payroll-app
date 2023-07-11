29ccd3f2a525a6365b9bb09572594cb0217345a3
bok1949 <bryan.vigilia.flores@gmail.com>
2023-07-11 23:27:23 +0800

    change in layout blade

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,322 +1,120 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-	<title>crud dashboard</title>
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="{{ URL::asset('assets/css/bootstrap.min.css') }}">
-	<!----css3---->
-	<link rel="stylesheet" href="{{ URL::asset('assets/css/custom.css') }}">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Payroll</title>
 
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="{{ URL::asset('new-assets/assets/css/bootstrap.css')}}">
 
-	<!--google fonts -->
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="{{ URL::asset('new-assets/assets/vendors/iconly/bold.css')}}">
 
-
-	<!--google material icon-->
-	<link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
-	{{--
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css" rel="stylesheet">
-	--}}
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-
+	<link rel="stylesheet" href="{{ URL::asset('new-assets/assets/vendors/perfect-scrollbar/perfect-scrollbar.css')}}">
+	<link rel="stylesheet" href="{{ URL::asset('new-assets/assets/vendors/bootstrap-icons/bootstrap-icons.css')}}">
+	<link rel="stylesheet" href="{{ URL::asset('new-assets/assets/css/app.css')}}">
+	<link rel="shortcut icon" href="{{ URL::asset('new-assets/assets/images/favicon.svg')}}" type="image/x-icon">
+	@stack('css-imports')
 </head>
 
 <body>
-
-
-
-	<div class="wrapper">
-
-		<div class="body-overlay"></div>
-
-		<!-------sidebar--design------------>
-
-		<div id="sidebar">
-			<div class="sidebar-header">
-				<h3><img src="img/logo.png" class="img-fluid" /><span>Company Name</span></h3>
-			</div>
-			<ul class="list-unstyled component m-0">
-				<li class="active disabled">
-					<a href="" class="dashboard"><i class="material-icons">dashboard</i>dashboard </a>
-				</li>
-
-				<li class="dropdown">
-					<a href="#homeSubmenu1" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-						<i class="material-icons">aspect_ratio</i>Manage Employee
-					</a>
-					<ul class="collapse list-unstyled menu" id="homeSubmenu1">
-						<li><a href="{{ route('employees.list') }}">View All Employee</a></li>
-						<li><a href="{{ route('employees.create') }}">Add employee</a></li>
-					</ul>
-				</li>
-
-
-				<li class="dropdown">
-					<a href="#homeSubmenu2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-						<i class="material-icons">apps</i>Attendance Log
-					</a>
-					<ul class="collapse list-unstyled menu" id="homeSubmenu2">
-						<li><a href="#">Pico</a></li>
-						<li><a href="#">Irisan</a></li>
-						<li><a href="#">Itogon Rescue</a></li>
-						<li><a href="#">Guisset</a></li>
-						<li><a href="#">Mt. Data</a></li>
-					</ul>
-				</li>
-
-				<li class="dropdown">
-					<a href="#homeSubmenu3" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-						<i class="material-icons">equalizer</i>Payslip
-					</a>
-					<ul class="collapse list-unstyled menu" id="homeSubmenu3">
-						<li><a href="#">Pico</a></li>
-						<li><a href="#">Irisan</a></li>
-						<li><a href="#">Itogon Rescue</a></li>
-						<li><a href="#">Guisset</a></li>
-						<li><a href="#">Mt. Data</a></li>
-					</ul>
-				</li>
-
-
-				<li class="dropdown">
-					<a href="{{ route('home.page') }}">
-						<i class="material-icons">equalizer</i>Home Page
-					</a>
-				</li>
-
-				<li class="dropdown">
-					<a href="{{ route('login.page') }}">
-						<i class="material-icons">equalizer</i>Login Page
-					</a>
-				</li>
-
-				{{-- <li class="dropdown">
-					<a href="#homeSubmenu4" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-						<i class="material-icons">extension</i>UI Element
-					</a>
-					<ul class="collapse list-unstyled menu" id="homeSubmenu4">
-						<li><a href="#">Pages 1</a></li>
-						<li><a href="#">Pages 2</a></li>
-						<li><a href="#">Pages 3</a></li>
-					</ul>
-				</li>
-
-				<li class="dropdown">
-					<a href="#homeSubmenu5" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-						<i class="material-icons">border_color</i>forms
-					</a>
-					<ul class="collapse list-unstyled menu" id="homeSubmenu5">
-						<li><a href="#">Pages 1</a></li>
-						<li><a href="#">Pages 2</a></li>
-						<li><a href="#">Pages 3</a></li>
-					</ul>
-				</li>
-
-				<li class="dropdown">
-					<a href="#homeSubmenu6" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-						<i class="material-icons">grid_on</i>tables
-					</a>
-					<ul class="collapse list-unstyled menu" id="homeSubmenu6">
-						<li><a href="#">table 1</a></li>
-						<li><a href="#">table 2</a></li>
-						<li><a href="#">table 3</a></li>
-					</ul>
-				</li>
-
-
-				<li class="dropdown">
-					<a href="#homeSubmenu7" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-						<i class="material-icons">content_copy</i>Pages
-					</a>
-					<ul class="collapse list-unstyled menu" id="homeSubmenu7">
-						<li><a href="#">Pages 1</a></li>
-						<li><a href="#">Pages 2</a></li>
-						<li><a href="#">Pages 3</a></li>
-					</ul>
-				</li>
-
-
-				<li class="">
-					<a href="#" class=""><i class="material-icons">date_range</i>copy </a>
-				</li>
-				<li class="">
-					<a href="#" class=""><i class="material-icons">library_books</i>calender </a>
-				</li> --}}
-
-			</ul>
-		</div>
-
-		<!-------sidebar--design- close----------->
-
-
-
-		<!-------page-content start----------->
-
-		<div id="content">
-
-			<!------top-navbar-start----------->
-
-			<div class="top-navbar">
-				<div class="xd-topbar">
-					<div class="row">
-						<div class="col-2 col-md-1 col-lg-1 order-2 order-md-1 align-self-center">
-							<div class="xp-menubar">
-								<span class="material-icons text-white">signal_cellular_alt </span>
-							</div>
+	<div id="app">
+		<div id="sidebar" class="active">
+			<div class="sidebar-wrapper active">
+				<div class="sidebar-header">
+					<div class="d-flex justify-content-between">
+						<div class="logo">
+							Company Name
 						</div>
-
-						{{-- <div class="col-md-5 col-lg-3 order-3 order-md-2">
-							<div class="xp-searchbar">
-								<form>
-									<div class="input-group">
-										<input type="search" class="form-control" placeholder="Search">
-										<div class="input-group-append">
-											<button class="btn" type="submit" id="button-addon2">Go
-											</button>
-										</div>
-									</div>
-								</form>
-							</div>
-						</div> --}}
-
-
-						<div class="col-10 col-md-6 col-lg-8 order-1 order-md-3">
-							<div class="xp-profilebar text-right">
-								<nav class="navbar p-0">
-									{{-- <ul class="nav navbar-nav flex-row ml-auto">
-										<li class="dropdown nav-item active">
-											<a class="nav-link" href="#" data-toggle="dropdown">
-												<span class="material-icons">notifications</span>
-												<span class="notification">4</span>
-											</a>
-											<ul class="dropdown-menu">
-												<li><a href="#">You Have 4 New Messages</a></li>
-												<li><a href="#">You Have 4 New Messages</a></li>
-												<li><a href="#">You Have 4 New Messages</a></li>
-												<li><a href="#">You Have 4 New Messages</a></li>
-											</ul>
-										</li>
-
-										<li class="nav-item">
-											<a class="nav-link" href="#">
-												<span class="material-icons">question_answer</span>
-											</a>
-										</li>
-
-										<li class="dropdown nav-item">
-											<a class="nav-link" href="#" data-toggle="dropdown">
-												<img src="img/user.jpg" style="width:40px; border-radius:50%;" />
-												<span class="xp-user-live"></span>
-											</a>
-											<ul class="dropdown-menu small-menu">
-												<li><a href="#">
-														<span class="material-icons">person_outline</span>
-														Profile
-													</a></li>
-												<li><a href="#">
-														<span class="material-icons">settings</span>
-														Settings
-													</a></li>
-												<li><a href="#">
-														<span class="material-icons">logout</span>
-														Logout
-													</a></li>
-
-											</ul>
-										</li>
-
-
-									</ul> --}}
-								</nav>
-							</div>
+						<div class="toggler">
+							<a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
 						</div>
-
 					</div>
-
-					<div class="xp-breadcrumbbar text-center">
-						<h4 class="page-title">Dashboard</h4>
-						<ol class="breadcrumb">
-							<li class="breadcrumb-item"><a href="#">Company</a></li>
-							<li class="breadcrumb-item active" aria-curent="page">Dashboard</li>
-						</ol>
-					</div>
-
-
 				</div>
-			</div>
-			<!------top-navbar-end----------->
+				<div class="sidebar-menu">
+					<ul class="menu">
+						<li class="sidebar-title">Menu</li>
 
+						<li class="sidebar-item active ">
+							<a href="" class='sidebar-link'>
+								<i class="bi bi-grid-fill"></i>
+								<span>Dashboard</span>
+							</a>
+						</li>
 
-			<!------main-content-start----------->
+						<li class="sidebar-item  has-sub">
+							<a href="#" class='sidebar-link'>
+								<i class="bi bi-stack"></i>
+								<span>Manage Employees</span>
+							</a>
+							<ul class="submenu ">
+								<li class="submenu-item ">
+									<a href="{{ route('employees.list') }}">Employees</a>
+								</li>
+								<li class="submenu-item ">
+									<a href="{{ route('employees.create') }}">Create</a>
+								</li>
+							</ul>
+						</li>
 
-			<div class="main-content ">
-				<div class="container-fluid">
-					@yield('main-content')
+						<li class="sidebar-item  has-sub">
+							<a href="#" class='sidebar-link'>
+								<i class="bi bi-collection-fill"></i>
+								<span>Attendance Log</span>
+							</a>
+							<ul class="submenu ">
+								<li class="submenu-item ">
+									<a href="">All</a>
+									@stack('sites-leftside-menu')
+								</li>
+							</ul>
+						</li>
+					</ul>
 				</div>
+				<button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
 			</div>
-
-			<!------main-content-end----------->
-
-
-
-
-
-
-
 		</div>
+		<div id="main">
+			<header class="mb-3">
+				<a href="#" class="burger-btn d-block d-xl-none">
+					<i class="bi bi-justify fs-3"></i>
+				</a>
+			</header>
 
+			<div class="page-heading">
+				@yield('page-heading')
+			</div>
+
+			<div class="page-content">
+				@yield('page-content')
+			</div>
+
+			<footer>
+				<div class="footer clearfix mb-0 text-muted">
+					<div class="float-start">
+						<p>2021 &copy; jeramel</p>
+					</div>
+				</div>
+			</footer>
+		</div>
 	</div>
+	<script src="{{ URL::asset('new-assets/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
+	<script src="{{ URL::asset('new-assets/assets/js/bootstrap.bundle.min.js')}}"></script>
 
-	<!----footer-design------------->
+	<script src="{{ URL::asset('new-assets/assets/vendors/apexcharts/apexcharts.js')}}"></script>
+	<script src="{{ URL::asset('new-assets/assets/js/pages/dashboard.js')}}"></script>
 
-	{{-- <footer class="footer">
-		<div class="container-fluid">
-			<div class="footer-in">
-				<p class="mb-0">&copy 2021 Vishweb Design . All Rights Reserved.</p>
-			</div>
-		</div>
-	</footer> --}}
-
-
-	<!-------complete html----------->
-
-
-
-
-
-
-	<!-- Optional JavaScript -->
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="{{ URL::asset('assets/js/jquery-3.3.1.slim.min.js') }}"></script>
-	<script src="{{ URL::asset('assets/js/jquery-3.3.1.min.js') }}"></script>
-	<script src="{{ URL::asset('assets/js/popper.min.js') }}"></script>
-	<script src="{{ URL::asset('assets/js/bootstrap.min.js') }}"></script>
+	<script src="{{ URL::asset('new-assets/assets/js/main.js')}}"></script>
+	@stack('js-imports')
 
 	<script type="text/javascript">
 		$(document).ready(function() {
-			$(".xp-menubar").on('click', function() {
-				$("#sidebar").toggleClass('active');
-				$("#content").toggleClass('active');
-			});
-
-			$('.xp-menubar,.body-overlay').on('click', function() {
-				$("#sidebar,.body-overlay").toggleClass('show-nav');
-			});
-
-			// $('#editEmployeeModal').modal('show');
+			
+			@stack('jq-code')
 		});
 	</script>
-
-	@yield('js')
-
-
 
 </body>
 
