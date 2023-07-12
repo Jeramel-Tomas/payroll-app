d88596e4e86817a796709efe90b31bb1303632cd
bok1949 <bryan.vigilia.flores@gmail.com>
2023-07-13 01:13:26 +0800

    Dashboard link set active when clicked

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -38,7 +38,7 @@
 					<ul class="menu">
 						<li class="sidebar-title">Menu</li>
 
-						<li class="sidebar-item active {{ (request()->is('')) ? 'active' : ''}}">
+						<li class="sidebar-item {{ (request()->is('/')) ? 'active' : ''}}">
 							<a href="{{ route('dashboard') }}" class='sidebar-link'>
 								<i class="bi bi-grid-fill"></i>
 								<span>Dashboard</span>
